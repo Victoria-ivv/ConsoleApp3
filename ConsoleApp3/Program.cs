f213b55fac1b60c7073c952c3544c17eba577b1b
Victoria-ivv <ivv83@me.com>
2021-03-10 11:34:56 +0300

    Финальное задание

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        var name = (Console.ReadLine());
+        Console.Write("Enter your age: ");
+        var age = (Console.ReadLine());
+		Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        Console.Write("Enter your birthdate: ");
+        var birthdate= (Console.ReadLine());
+        Console.WriteLine("Your birthdate is {0}",  birthdate);
+        Console.Write("What is your favorite day of week?");
+        DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
+        Console.WriteLine("Your favorite day is {0}",day);
+        Console.ReadKey();
+    }
+}
